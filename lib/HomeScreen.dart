ba4311eaf4df91ad70136f9a02e98bde147f7db9
manan gandhi <manangandhi@manans-Mac-mini.local>
2023-05-24 17:34:06 -0400

    designed Home Screen

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:resell_app/SignupScreen/signUpScreen.dart';
@@ -9,6 +10,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:resell_app/uploadAdScreen.dart';
+import 'package:timeago/timeago.dart' as tAgo;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -19,6 +21,18 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  QuerySnapshot? items;
+  getMyData() {
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .get()
+        .then((results) {
+      setState(() {
+        getUserName = results.data()?['userName'];
+      });
+    });
+  }
 
   FirebaseAuth auth = FirebaseAuth.instance;
   Future<void> requestLocationPermission() async {
@@ -58,7 +72,7 @@ class _HomeScreenState extends State<HomeScreen> {
     Position newPosition = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high,
     );
-    position=newPosition;
+    position = newPosition;
     List<Placemark> placemarks = await placemarkFromCoordinates(
       newPosition.latitude,
       newPosition.longitude,
@@ -69,7 +83,6 @@ class _HomeScreenState extends State<HomeScreen> {
         '${placemark.street} ${placemark.locality} ${placemark.postalCode} ${placemark.country}';
     completeAddress = newCompleteAddress.trim();
 
-
     return completeAddress;
   }
 
@@ -77,67 +90,151 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     requestLocationPermission();
+    userId = FirebaseAuth.instance.currentUser!.uid;
+    userEmail = FirebaseAuth.instance.currentUser!.email!;
+    FirebaseFirestore.instance
+        .collection('items')
+        .where("status", isEqualTo: "approved")
+        .orderBy("time", descending: true)
+        .get()
+        .then((results) {
+      setState(() {
+        items = results;
+        print("*" * 100);
+        print(items?.docs.length);
+        print("*" * 100);
+      });
+    });
+    getMyData();
   }
 
   @override
   Widget build(BuildContext context) {
     double _screenWidth = MediaQuery.of(context).size.width,
         _screenHeight = MediaQuery.of(context).size.height;
-    Widget? showItemList() {}
+    Widget? showItemList() {
+      if (items != null) {
+        return ListView.builder(
+            itemCount: items!.docs.length,
+            padding: const EdgeInsets.all(8.0),
+            itemBuilder: (context, i) {
+              return Container(
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: GestureDetector(
+                          onTap: () {},
+                          child: Text(
+                            items?.docs[i].get('userName'),
+                          ),
+                        ),
+                        trailing: items?.docs[i].get('uid') == userId
+                            ? Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  GestureDetector(
+                                    onTap: () {},
+                                    child: const Icon(
+                                      Icons.edit_outlined,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
+                                  GestureDetector(
+                                    onDoubleTap: () {},
+                                    child:
+                                        const Icon(Icons.delete_forever_sharp),
+                                  )
+                                ],
+                              )
+                            : const Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [],
+                              ),
+                      ),
+                      GestureDetector(
+                        onDoubleTap: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Container(
+                            height: 150,
+                            child: Image.network(
+                                items?.docs[i].get("urlImage1"),
+                                fit: BoxFit.fill),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: Text(
+                          "\$${items?.docs[i].get("itemPrice")}",
+                          style: const TextStyle(
+                            letterSpacing: 2.0,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.image_sharp,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 10.0),
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      items?.docs[i].get("itemModel"),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.watch_later_outlined,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 10.0),
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(tAgo.format(
+                                        (items?.docs[i].get('time')).toDate())),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            });
+      } else {
+        return (Text("Loading"));
+      }
+    }
 
     return Scaffold(
       key: _scaffoldKey,
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
       body: SafeArea(
-        child: Column(
-          children: [
-         Padding(
-              padding: const EdgeInsets.only(top: 15, left: 9, right: 9),
-              child: Row(
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.blueAccent,
-                    ),
-                    padding: const EdgeInsets.all(3),
-                    child: GestureDetector(
-                      onTap: () {
-                        _scaffoldKey.currentState?.openDrawer();
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(3.0),
-                        child: const Icon(
-                          Icons.menu,
-                          weight: 12,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: SizedBox(),
-            ),
-            Center(
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * 0.4,
-                child: completeAddress == null
-                    ? const Text("")
-                    : Text(
-                        completeAddress,
-                        style: const TextStyle(color: Colors.black),
-                      ),
-              ),
-            ),
-            Center(
-              child: SizedBox(
-                width: _screenWidth,
-                child: showItemList(),
-              ),
-            ),
-          ],
+        child: Center(
+          child: Container(
+            width: _screenWidth,
+            child: showItemList(),
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
